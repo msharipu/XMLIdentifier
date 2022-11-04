33c8e6d1503bbcc8b1ec0a4737111038f948e9c1
Muhammad Aliff Romi Sharipudin <maliffromi@gmail.com>
2022-11-05 07:36:53 +0800

    added feature to find the opening element  node. using substring to capture the element name.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,64 @@ namespace XMLIdentifier
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            string test1 = "<Design><Code>hello world</Design></Code>";
+
+
+            p.DetermineSxml(test1);
+            Console.ReadKey();
+        }
+
+        bool DetermineSxml(string sxml)
+        {
+            List<string> startElement = new List<string>();
+            List<string> endElement = new List<string>();
+            bool invalidXmlDetected = false;
+
+            for(int i = 0; i < sxml.Length; i++)
+            {
+                if (invalidXmlDetected)
+                    break;
+
+                int opIndex = 0;
+                int edIndex = 0;
+
+                if(sxml[i] == '<')
+                {
+                    opIndex = i + 1;
+
+                    if ((sxml[i] + 1) != '/') // this is an opening element
+                    {
+                        for (int j = i+1; j < sxml.Length; j++)
+                        {
+                            if (sxml[j] != '>')
+                                continue;
+                            else if(sxml[j] == '<') // if found another opening bracket, then this is an invalid
+                            {
+                                invalidXmlDetected = true;
+                                break;
+                            }
+                            else // found '>', get the index
+                            {
+                                edIndex = j -1 ;
+                                break;
+                            }
+                        }
+
+                        if(edIndex == 0) // does not found any closing bracket, invalid
+                            invalidXmlDetected = true;
+                        else
+                        {
+                            Console.WriteLine(sxml.Substring(opIndex, edIndex));
+                        }
+                    }
+                }
+
+                break;
+                
+            }
+
+            return true;
         }
     }
 }
